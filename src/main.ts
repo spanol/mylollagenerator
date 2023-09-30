f32d6d40c0996997c5756200c72c3ad594629fad
code_spanol <85651178+spanol@users.noreply.github.com>
2023-09-30 15:26:05 -0300

    General Files update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 import { store, key } from '@/store';
 
-import 'bootstrap'
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const app = createApp(App)
 app.use(router)
