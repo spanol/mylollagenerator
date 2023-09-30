1a89735d29a3755c35f269b0df7945db093f8319
code_spanol <85651178+spanol@users.noreply.github.com>
2023-09-30 15:26:18 -0300

    Updates on app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-link to="/login">Login</router-link>
-  </nav>
+  <Header></Header>
   <router-view/>
+  <Footer></Footer>
 </template>
 
 <style>
 
 </style>
+
+<script lang="ts" setup>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+</script>
