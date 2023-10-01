b7d66ee84c6fdeecf78ed353766bc2206997a6d1
code_spanol <85651178+spanol@users.noreply.github.com>
2023-10-01 18:19:49 -0300

    Added vue flicking

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <Header></Header>
-  <router-view/>
-  <Footer></Footer>
+  <Header />
+  <div class="view d-flex justify-content-center align-items-center">
+    <router-view />
+  </div>
+  <Footer />
 </template>
 
 <style>
+body,
+html {
+  min-height: 100vh !important;
+  background-color: black !important;
+  color: white !important;
+}
 
+.view {
+  min-height: 400px;
+}
 </style>
 
 <script lang="ts" setup>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { store, key } from '@/store';
+import Flicking from "@egjs/vue3-flicking";
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import "@egjs/vue3-flicking/dist/flicking.css";
+import "@egjs/vue3-flicking/dist/flicking-inline.css";
 
 const app = createApp(App)
 app.use(router)
 app.use(store, key);
+app.component("Flicking", Flicking)
 app.mount('#app')
